75f29e13d53da65995cbfce5a2380c04106c0630
Zach Goldberg <zach@zachgoldberg.com>
2024-03-15 10:59:20 -0700

    simple scripts for testing tenv install

---
+++ test/run_tenv_test.sh
@@ -0,0 +1,8 @@
+
+# Ubuntu
+docker build -t ubuntu-tenv-test --target ubuntu .
+docker run ubuntu-tenv-test
+
+## Alpine
+# docker build -t alpine-tenv-test --target alpine .
+# docker run alpine-tenv-test
