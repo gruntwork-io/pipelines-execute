2614887a7dd71bc8b4d589edd4463dfa8d6eeed6
Zach Goldberg <zach@zachgoldberg.com>
2024-03-15 11:49:46 -0700

    remove now-unnecessary tests

--- test/run_tenv_test.sh
+++
@@ -1,8 +0,0 @@
-
-# Ubuntu
-docker build -t ubuntu-tenv-test --target ubuntu .
-docker run ubuntu-tenv-test
-
-## Alpine
-docker build -t alpine-tenv-test --target alpine .
-docker run alpine-tenv-test
